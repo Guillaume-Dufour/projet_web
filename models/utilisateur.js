02d74a12fc50e84e6024f1ee8797452a7835e61a
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2020-02-25 00:16:35 +0100

    login en cours

--- models/utilisateur.js
+++ models/utilisateur.js
@@ -3,22 +3,46 @@ var connexion = require('../config/db');
 class Utilisateur {
 
     static create(data) {
-        /*connexion.query("INSERT INTO utilisateur (nom_utilisateur, prenom_utilisateur, mail_utilisateur, sexe_utilisateur, type_utilisateur, password_utilisateur, telephone_utilisateur) " +
-            "VALUES (?)", data.map(), function (err, result) {
-
-            if (err) {
-                throw err;
-            }
-
-        })*/
-
         connexion.query("INSERT INTO utilisateur SET ?", data, function (err, result) {
-
             if (err) {
                 throw err;
             }
 
-            console.log("result1 = "+result)
+        })
+    }
+
+    static exist(mail, cb) {
+        connexion.query("SELECT count(*) as nb FROM utilisateur WHERE mail_utilisateur = ?", [mail], function (err, rows) {
+
+            if (err) {
+                throw err;
+            } else {
+                cb(rows[0].nb);
+            }
+        })
+    }
+
+    static getUserByMail(mail, cb) {
+        connexion.query("SELECT * FROM utilisateur WHERE mail_utilisateur = ?", [mail], function (err, rows) {
+            if (err) {
+                throw err;
+            }
+            else {
+                if (rows[0]) {
+                    cb(rows[0]);
+                }
+                else {
+                    cb(0);
+                }
+            }
+        })
+    }
+
+    static all() {
+        connexion.query("SELECT * FROM utilisateur", function (err, result) {
+            if (err) throw err;
+
+            console.log(result);
 
         })
     }
@@ -41,21 +65,6 @@ class Utilisateur {
 
         return promess;
     }*/
-
-    function exist(mail) {
-        connexion.query("SELECT * FROM utilisateur WHERE mail_utilisateur = ?", [mail], function (err, result) {
-            console.log((result));
-        })
-    }
-
-    static all() {
-        connexion.query("SELECT * FROM utilisateur", function (err, result) {
-            if (err) throw err;
-
-            console.log(result);
-
-        })
-    }
 
 }
 
--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -1,48 +1,64 @@
 var express = require('express');
 var router = express.Router();
 var Utilisateur = require('../models/utilisateur');
+let bcrypt = require('bcrypt');
 
 router.route('/inscription')
     .get(function (req, res) {
-    res.render('inscription');
-})
+    res.render('inscription', {error: ""});
+    })
 
-.post(function (req,res) {
-    var data = {
-        nom_utilisateur: req.body.nom,
-        prenom_utilisateur: req.body.prenom,
-        mail_utilisateur: req.body.mail,
-        sexe_utilisateur: req.body.sexe,
-        type_utilisateur: 3,
-        password_utilisateur: req.body.password,
-        telephone_utilisateur: req.body.telephone
-    };
+    .post(function (req,res) {
+        var data = {
+            nom_utilisateur: req.body.nom,
+            prenom_utilisateur: req.body.prenom,
+            mail_utilisateur: req.body.mail,
+            sexe_utilisateur: req.body.sexe,
+            type_utilisateur: 3,
+            password_utilisateur: bcrypt.hashSync(req.body.password,10),
+            telephone_utilisateur: req.body.telephone
+        };
 
-    var exist = Utilisateur.exist(data.mail_utilisateur);
 
-    console.log(exist);
 
-    if (exist == 1) {
-        res.send("Salut")
-    }
-    else {
-
-        Utilisateur.create(data);
-        res.redirect('/');
-    }
-});
-
-router.route('/test')
-    .get(async function(req, res) {
-        var exist = await Utilisateur.exist("guillaume@egsb.fr");
-        console.log(exist)
-
+        Utilisateur.exist(data.mail_utilisateur, function (result) {
+            if (result == 1) {
+                let error = "Mail déjà existant"
+                res.render('inscription', { error : "Mail déjà existant"});
+            }
+            else {
+                Utilisateur.create(data);
+                res.redirect('/users/inscription');
+            }
+        });
     });
 
-router.get('/guillaume', function (req, res) {
-    Utilisateur.all();
-    res.send("Bonjoru")
+router.route('/login')
+    .get(function (req, res) {
+        res.render('login')
+    })
+    .post(function (req, res) {
+        Utilisateur.getUserByMail(req.body.mail, function (result) {
+            console.log(result);
+            if (result == 0) {
+                res.redirect('/users/login');
+            }
+            else {
+                bcrypt.compare(req.body.password, result.password_utilisateur, function (err, success) {
+                    if (err)
+                        throw err;
 
-})
+                    if (success) {
+                        res.send("Authentification réussi");
+                    }
+                    else {
+                        res.send('NOOON');
+                    }
+                })
+            }
+        })
+
+
+    })
 
 module.exports = router;
