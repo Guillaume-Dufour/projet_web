62660209ccdfbcc03db8ef0adf9b5b803318cc57
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2020-03-07 13:03:47 +0100

    Test 1 heroku

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
-var connexion = mysql.createConnection({
+/*var connexion = mysql.createConnection({
     database: 'projet_web',
     host: 'localhost',
     user: 'root',
     password: ''
+});*/
+
+var connexion = mysql.createPool({
+    database: 'heroku_e8bbb531c7959a6',
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b8ea25e5578da9',
+    password: '0f9a566c'
 });
 
 connexion.connect(function (err) {
